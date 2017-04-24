b96900bf79c56a498af59b97714fcfe2144275e4
Gustavo Mantovan <gumantovan@gmail.com>
2017-04-24 19:35:54 -0400

    graph working used SQL commands

--- app/helpers/mails_helper.rb
+++ app/helpers/mails_helper.rb
@@ -1,2 +1,41 @@
 module MailsHelper
+
+  def chart_stacked_company_name
+    sql = 'SELECT COUNT(*) AS count_all, mails.size, companies.name
+    FROM mails
+    INNER JOIN companies ON (
+      companies.id = mails.company_id
+    )
+    GROUP BY mails.company_id, mails.size;'
+
+    data = {}
+
+    Mail.connection.select_all(sql).rows.each {|row|
+      data[row[1]] ||= []
+      data[row[1]] << [row[2], row[0]]
+    }
+
+    mails = data.keys.map {|name|
+      {
+        name: name,
+        data: data[name]
+      }
+    }
+
+  column_chart mails, stacked: true, height: '500px', library: {
+      title: {text: 'Mails per Company', x: -20},
+      yAxis: {
+          allowDecimals: false,
+          title: {
+              text: 'Count'
+          }
+      },
+      xAxis: {
+          title: {
+              text: 'Company Name'
+          }
+      }
+  }
+  end
+
 end
--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,8 @@
 class Company < ApplicationRecord
   has_many :mails
+
+  def company_size
+    Mail.where(company:self).count
+  end
+  
 end
