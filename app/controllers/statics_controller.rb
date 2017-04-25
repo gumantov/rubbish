d33a38b5f83db26771e41781d8561a76b78a4946
Gustavo Mantovan <gumantovan@gmail.com>
2017-04-25 16:53:24 -0400

    added static controller and static pages

---
+++ app/controllers/statics_controller.rb
@@ -0,0 +1,9 @@
+class StaticsController < ApplicationController
+
+  def about
+  end
+
+  def contact
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root 'mails#index'
   resources :companies
-  get "mails/about"
   resources :mails
+  #static pages routes
+  get "statics/about" => 'statics#about'
+  get "statics/contact" => 'statics#contact'
+
+  #devise conf
   devise_for :users
-
-
-  resources :products do
-    get :autocomplete_company_name, :on => :collection
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
